2e9ecafdaa70236e272a17a26aee64fece573039
Mike Santos <mike.santos@contraprova.com.br>
2024-02-15 20:20:35 -0300

    [Development] - feature: create logo component

--- src/components/logo/Logo.stories.ts
+++ src/components/logo/Logo.stories.ts
@@ -3,11 +3,24 @@ import Logo from '.'
 
 const meta: Meta<typeof Logo> = {
   component: Logo,
+  argTypes: {
+    color: {
+      options: ['colorized', 'white'],
+      control: { type: 'radio' },
+    },
+    size: {
+      options: ['small', 'medium', 'large'],
+      control: { type: 'radio' },
+    },
+  },
 }
 
 export default meta
 type Story = StoryObj<typeof Logo>
 
-export const ColorizedLogo: Story = {
-  args: {},
+export const Default: Story = {
+  args: {
+    color: 'white',
+    size: 'medium',
+  },
 }
