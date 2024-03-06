4c14dfe363d2db01e1677bbba5274bebe24dd279
Mike Santos <mike.santos@contraprova.com.br>
2024-03-06 18:10:59 -0300

    [Development] - refactor: change dashboard layout

--- src/app/painel-controle/page.tsx
+++ src/app/painel-controle/page.tsx
@@ -1,4 +1,6 @@
 import BaseDefault from '@/components/Base/BaseDefault'
+import TableArea from '@/components/TableArea/TableArea'
+import TitleArea from '@/components/TitleArea/TitleArea'
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
@@ -7,19 +9,11 @@ export const metadata: Metadata = {
 
 const PainelControle = () => (
   <BaseDefault>
-    <div className="bg-white">
-      <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 className="text-2xl font-bold tracking-tight text-slate-700">
-          Painel de Controle
-        </h1>
-      </div>
-    </div>
+    <TitleArea title="Painel de Controle" />
 
-    <section>
-      <div className="mx-auto max-w-7xl py-8 sm:px-6 lg:px-8">
-        <p>Widget</p>
-      </div>
-    </section>
+    <TableArea>
+      <p>Área do Painel de Controle</p>
+    </TableArea>
   </BaseDefault>
 )
 
