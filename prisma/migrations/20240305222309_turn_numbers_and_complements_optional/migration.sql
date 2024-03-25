6c92a2137f5ba6d371443054cc81f868c7844ecc
Mike Santos <iddeiasimplesmike@gmail.com>
2024-03-25 11:07:01 -0300

    [Development] - chore: delete migrations and tables of prisma folder

--- prisma/migrations/20240305222309_turn_numbers_and_complements_optional/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "ConstructionManager" ALTER COLUMN "number" DROP NOT NULL,
-ALTER COLUMN "complement" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "EmbargoedWorks" ALTER COLUMN "number" DROP NOT NULL,
-ALTER COLUMN "complement" DROP NOT NULL;
