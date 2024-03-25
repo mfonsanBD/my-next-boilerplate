11949702519c5272eb38264bd1102ac862353105
Mike Santos <iddeiasimplesmike@gmail.com>
2024-03-25 11:02:46 -0300

    [Development] - chore: turn app to default configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import AuthBase from '@/components/AuthBase/AuthBase'
-import LoginForm from '@/components/LoginForm/LoginForm'
-
 export default function Home() {
   return (
-    <AuthBase>
-      <LoginForm />
-    </AuthBase>
+    <div>
+      <h1>Home</h1>
+    </div>
   )
 }
